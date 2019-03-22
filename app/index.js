d297c8ee9fe784698f211b45da6785259ee39418
nataliasu <suszynska.nat@gmail.com>
2019-03-22 10:03:52 +0100

    change code into async/await and add user display

--- app/index.js
+++ app/index.js
@@ -18,66 +18,74 @@ const ending =
     `   </tbody>
     </table>`
 
-fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(users => {
-        //filling usersList
-        for (let i = 0; i < users.length; i++) {
-            usersList.push({
-                name: users[i].name,
-                email: users[i].email,
-                companyUri: users[i].uris && users[i].uris.company,
-                companyNumber: users[i].uris.company && users[i].uris.company.substring(11)
-            });
+const init = async () => {
+    const response = await fetch('http://localhost:3000/users')
+    const users = await response.json();
+    for (let i = 0; i < users.length; i++) {
+        usersList.push({
+            name: users[i].name,
+            email: users[i].email,
+            companyUri: users[i].uris && users[i].uris.company,
+            companyNumber: users[i].uris.company && users[i].uris.company.substring(11)
+        });
+    }
+    const res = await fetch('http://localhost:3000/companies');
+    const companies = await res.json();
+    for (let i = 0; i < companies.length; i++) {
+        companiesList.push({
+            name: companies[i].name,
+            uri: companies[i].uri,
+            users: []
+        });
+    }
+    //pushing usersList into companiesList.users
+    for (let i = 0; i < usersList.length; i++) {
+        const userIndex = usersList[i].companyNumber;
+        const userEmail = usersList[i].email;
+        companiesList[userIndex].users.push(userEmail);
+    }
+    // sorting the list
+    companiesList.sort((a, b) => {
+        const one = a.users.length;
+        const two = b.users.length;
+        if (one > two) {
+            return -1;
+        } else if (one < two) {
+            return 1;
         }
-
-        fetch('http://localhost:3000/companies')
-            .then(response => response.json())
-            .then(companies => {
-                //filling companiesList
-                for (let i = 0; i < companies.length; i++) {
-                    companiesList.push({
-                        name: companies[i].name,
-                        uri: companies[i].uri,
-                        users: []
-                    });
-                }
-            })
-
-            .then(() => {
-                //pushing usersList into companiesList.users
-                for (let i = 0; i < usersList.length; i++) {
-                    const userIndex = usersList[i].companyNumber;
-                    const userName = usersList[i].name;
-                    companiesList[userIndex].users.push(userName);
-                }
-
-                // sorting the list
-                companiesList.sort((a, b) => {
-                    const one = a.users.length;
-                    const two = b.users.length;
-                    if (one > two) {
-                        return -1;
-                    } else if (one < two) {
-                        return 1;
-                    }
-                    return 0;
-                });
-            })
-            //        console.log(companiesList);
-            .then(() => {
-                let tableText = table;
-                //adding input into table
-                for (let i = 0; i < companiesList.length; i++) {
-                    tableText +=
-                        `<tr class="table-light">
-                        <td>${i}</td>
-                        <td>${companiesList[i].name}</td>
-                        <td>${companiesList[i].uri}</td>
-                        </tr>`;
-
-                }
-                tableText += ending;
-                document.getElementById('table').innerHTML = tableText;
-            });
+        return 0;
     });
+    //        console.log(companiesList);
+    let tableText = table;
+    //adding input into table
+    for (let i = 0; i < companiesList.length; i++) {
+        tableText +=
+            `<tr class="table-light">
+                <td>${i}</td>
+                <td>${companiesList[i].name}</td>
+                <td>${companiesList[i].uri}</td>
+            </tr>`;
+        if (companiesList[i].users.length) {
+            tableText +=
+                `<tr>
+                    <th scope="col">User index</th>
+                    <th scope="col" colspan="2">Email</th>
+                </tr>`
+
+            for (let j = 0; j < companiesList[i].users.length; j++) {
+                let row = `${j+1}`;
+                tableText +=
+                    `<tr id=${row}" class="table-info">
+                            <td>${row}</td>
+                            <td colspan="2">${companiesList[i].users[j]}</td>
+                        </tr>`
+            }
+
+        }
+    }
+
+    tableText += ending;
+    document.getElementById('table').innerHTML = tableText;
+}
+
+init();
